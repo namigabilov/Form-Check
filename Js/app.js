e41ec4fbd78bc8c74285337df1ccae73957697f1
namigabilov <namigna@code.edu.az>
2022-11-26 21:27:17 +0400

    All Changes

--- Js/app.js
+++ Js/app.js
@@ -1,34 +1,62 @@
-var button = document.getElementById('button');
-var hover = document.getElementsByClassName('hover');
-
-button.onclick = function () {
-    var email = document.getElementById("email").value ;
-    var nameInput = document.getElementById("name").value ;
-    var passwordInp = document.getElementById("password").value ;
-    var patternForEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    var resultForEmail = patternForEmail.test(email);
-    var patternForName = /\w*[A-Z]\w*[A-Z]\w*/;
-    var resultForName = patternForName.test(nameInput);
-    var patternForPassword = /(?=.*[A-Z]\w{0}[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
-    var resultForPassword = patternForPassword.test(passwordInp);
-    if (resultForEmail) {
-       document.getElementById('invalidEmail').style.opacity = '0';
-    }
-   else{
-       document.getElementById('invalidEmail').style.opacity = '1';
-        
-    }
-    if (resultForName) {
-        document.getElementById("invalidName").style.opacity ='0'
+let button = document.getElementById('button');
+let hover = document.getElementsByClassName('hover');
+function SignIn(){
+    let inputValueName = document.getElementById("name1").value ;
+    let inputValueEmail = document.getElementById('email1').value ;
+    let inputValuePassword = document.getElementById('password1').value ;
+    let info = JSON.parse(localStorage.getItem('info'));
+    var checkName = false;
+    var checkPassword = false;
+    var checkEmail= false;
+    info.forEach(info => {
+        var username = info.Username ;
+        var password = info.Password 
+        var email = info.Email 
+        if (username === inputValueName) {
+            document.getElementById('invalidName').style.opacity = '0';
+            checkName =true;
+        }
+        else if (inputValueName === ""){
+            document.getElementById('invalidName').style.opacity = '0';
+        }
+        else{
+            document.getElementById('invalidName').style.opacity = '1';
+        }
+        if (password === inputValuePassword) {
+            document.getElementById('invalidPassword').style.opacity = '0';
+            checkPassword = true;
+        }
+        else if( inputValuePassword === "" ){
+            document.getElementById('invalidPassword').style.opacity = '0';
+        }
+        else{
+            document.getElementById('invalidPassword').style.opacity = '1';
+        }
+        if (email === inputValueEmail) {
+            document.getElementById('invalidEmail').style.opacity = '0';
+            checkEmail = true;
+        }
+        else if( inputValueEmail === "" ){
+        document.getElementById('invalidEmail').style.opacity = '0';
+        }
+        else{
+            document.getElementById('invalidEmail').style.opacity = '1';
+        }
+        if (checkEmail && checkName && checkPassword) {
+            alert('Welcome!');
+            window.close();
+            location.reload()
+        }
+    });
+}
+let eye = document.getElementById('eye');
+eye.onclick =function(){
+    if (eye.classList.contains('fa-eye')) {
+        eye.classList.replace('fa-eye','fa-eye-slash');
+        document.getElementById('password1').type = 'text'
     }
     else{
-        document.getElementById("invalidName").style.opacity ='1'
+        eye.classList.replace('fa-eye-slash','fa-eye');
+        document.getElementById('password1').type = 'password'
     }
-    if (resultForPassword) {
-        document.getElementById("invalidPassword").style.opacity = '0'
-    }
-    else{
-        document.getElementById("invalidPassword").style.opacity = '1'
-    }
-
 }
--- Js/register.js
+++ Js/register.js
@@ -1,3 +1,90 @@
 if (localStorage.getItem('info') === null){
     localStorage.setItem('info',JSON.stringify([]));
 }
+var register = document.getElementById('register');
+var passwordCheck = false;
+register.onclick = function(){
+    let email = document.getElementById("email").value ;
+    let nameInput = document.getElementById("name").value ;
+    let passwordInp = document.getElementById("password").value ;
+    let retypePassword =document.getElementById("retypePassword").value;
+    let patternForEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    let resultForEmail = patternForEmail.test(email);
+    let patternForName = /\w*[A-Z]\w*/;
+    let resultForName = patternForName.test(nameInput);
+    let patternForPassword = /(?=.*[A-Z]\w{0}[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    let resultForPassword = patternForPassword.test(passwordInp);
+    if (resultForEmail) {
+        document.getElementById('invalidEmail').style.opacity = '0';
+    }
+    else{
+       
+        document.getElementById('invalidEmail').style.opacity = '1';
+        
+    }
+    if (resultForName) {
+        document.getElementById("invalidName").style.opacity ='0'
+    }
+    else{
+        
+        document.getElementById("invalidName").style.opacity ='1'
+    }
+    if (resultForPassword) {
+        document.getElementById("invalidPassword").style.opacity = '0'
+    }
+    else{
+        
+        document.getElementById("invalidPassword").style.opacity = '1'
+    }
+    let passwordCheck = (passwordInp !== retypePassword);
+    if(passwordCheck){
+        document.getElementById('notSamePassword').style.opacity = '1'
+    }
+    else{
+        document.getElementById('notSamePassword').style.opacity = '0'
+        
+    }
+    
+    let info = JSON.parse(localStorage.getItem('info'));
+    
+    let someUser = info.find(x=>x.Username === nameInput);
+    let someEmail = info.find(x=>x.Email === email);
+
+    if (someUser === undefined && someEmail === undefined && resultForName && resultForEmail && !passwordCheck) {
+        info.push({
+            Username : nameInput,
+            Email : email,
+            Password : passwordInp
+        });
+        alert('User added!');
+        location.reload();
+    }
+    else{
+       if (someUser !== undefined) {
+        document.getElementById('invalidName').innerHTML = 'This name already exists';
+        document.getElementById("invalidName").style.opacity ='1'
+       }
+       if (someEmail !== undefined) {
+        document.getElementById('invalidEmail').innerHTML = 'This email already exists';
+        document.getElementById('invalidEmail').style.opacity = '1';
+        console.log("Some Email");
+       }
+       
+    }
+    
+    localStorage.setItem('info',JSON.stringify(info))
+    let foward = document.getElementById('foward');
+    
+}
+let passwordType = document.getElementById('password');
+let eye = document.getElementById('eye');
+eye.onclick =function(){
+    if (eye.classList.contains('fa-eye')) {
+        eye.classList.replace('fa-eye','fa-eye-slash');
+        passwordType.type = 'text'
+    }
+    else{
+        eye.classList.replace('fa-eye-slash','fa-eye');
+        passwordType.type = 'password'
+    }
+}
